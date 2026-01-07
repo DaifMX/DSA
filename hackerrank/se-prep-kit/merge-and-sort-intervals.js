c5fd53123587cbb410aca6acba0bbd00271164fb
Daif <applediegoaif@gmail.com>
2026-01-06 20:19:45 -0600

    add(hackerrank/se-prep-kit): Merge and sort intervals.

---
+++ hackerrank/se-prep-kit/merge-and-sort-intervals.js
@@ -0,0 +1,27 @@
+function mergeHighDefinitionIntervals(intervals) {
+    if (!intervals.length) return [];
+    
+    intervals.sort((a, b) => a[0] - b[0]);
+    
+    const res = [intervals[0]];
+    
+    if (intervals.length === 1) return res;
+    
+    for (let i = 1; i < intervals.length; i++) {        
+        const lastInterval = res[res.length - 1];
+        const start_LastInterval = lastInterval[0];
+        const end_LastInterval = lastInterval[1];
+        
+        const currInterval = intervals[i];
+        const start_CurrInterval = currInterval[0];
+        const end_CurrInterval = currInterval[1];
+        
+        if (start_CurrInterval <= end_LastInterval) {
+            res[res.length - 1] = [start_LastInterval, Math.max(end_CurrInterval, end_LastInterval)];
+        } else {
+            res.push(intervals[i]);
+        }
+    }
+    
+    return res;
+}

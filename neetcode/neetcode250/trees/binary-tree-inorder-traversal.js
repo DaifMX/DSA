ab58c9dc48c873f53d91340cd63f3b51da90ba40
Daif <applediegoaif@gmail.com>
2026-01-24 22:33:27 -0600

    add(neetcode/neetcode250): Binary Tree Inorder Traversal

---
+++ neetcode/neetcode250/trees/binary-tree-inorder-traversal.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+
+    inorderTraversal(root) {
+        const res = [];
+        
+        const fun = (root) => {
+            if (!root) return;
+
+            fun(root.left);
+            res.push(root.val);
+            fun(root.right);
+        }
+
+        fun(root);
+        return res; 
+    }
+}

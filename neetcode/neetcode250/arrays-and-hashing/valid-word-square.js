b91f7750a351a73411c7c46081c3683448399e4c
Daif <applediegoaif@gmail.com>
2026-02-25 13:15:40 -0600

    add(neetcode/neetcode250): Valid word square.

---
+++ neetcode/neetcode250/arrays-and-hashing/valid-word-square.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} words
+     * @return {boolean}
+     */
+    validWordSquare(words) {
+        for (let i = 0; i < words.length; i++) {
+            for (let j = 0; j < words[i].length; j++) {
+                if (j >= words.length ||
+                    i >= words.length ||
+                    words[i][j] != words[j][i]
+                ) return false;
+            }
+        }
+
+        return true;
+    }
+}
